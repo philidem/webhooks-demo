c2721bcb5b3160b59baa5e0cdf7bae46fa7ce7cc
Phillip Gates-Idem <phillip.idem@gmail.com>
2015-06-02 10:11:44 -0400

    Upgrading packages

--- run-server.sh
+++ run-server.sh
@@ -1,4 +1,4 @@
 ulimit -S -n 2048
 
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-node $BASEDIR/node_modules/.bin/browser-refresh "$BASEDIR/server.js" $@
+MARKO_CLEAN=true node $BASEDIR/node_modules/.bin/browser-refresh "$BASEDIR/server.js" $@
